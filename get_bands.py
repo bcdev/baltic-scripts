152faf6a6faed2bbd4ba29ef75b836064e574810
dagmarM <Dagmar.Mueller@brockmann-consult.de>
2019-05-22 16:21:57 +0200

    - adjusted the get_bands routine: now it includes bands_forwardNN, bands_abs - a few arrays with wavelengths for S2 have been added.

--- get_bands.py
+++ get_bands.py
@@ -13,14 +13,22 @@ def main(sensor,proc):
         bands_rw   = [400,412,443,490,510,560,620,665,674,681,709,754,            779,865,885,        1020]
         bands_corr = [    412,443,490,510,560,620,665,674,681,    754] # forwardNN but 400 and 709
         bands_chi2 = [400,412,443,490,510,560,620,665,674,681,    754] # forwardNN but 709
+        bands_forwardNN = [400, 412, 443, 490, 510, 560, 620, 665, 674, 681, 709, 754]
+        bands_abs = [760, 764, 767, 900, 940]
         #bands_corr = [    412,443,490,510,560,620,665,            754,            779,865                 ] # POLYMER
         #bands_corr = [400,412,443,490,510,560,620,665,674,681,709,754,            779,865,            1020] #C2RCC
-    #elif sensor == 'S2' #TODO
+    elif sensor == 'S2':
+        #TODO add the missing wavelengths
+        bands_sat = [443, 490, 560, 665, 705, 740, 783, 842, 865, 945, 1375, 1610, 2190]
+        bands_rw  = []
+        bands_corr = []
+        bands_forwardNN = [443, 490, 560, 665, 705, 740]
+        bands_abs = []
     else:
         print( "Unknown sensor %s"%sensor)
         sys.exit(1)
 
-    return(bands_sat, bands_rw, bands_corr, bands_chi2)
+    return(bands_sat, bands_rw, bands_corr, bands_chi2, bands_forwardNN, bands_abs)
 
 if __name__ == "__main__":
     import sys
