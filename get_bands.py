d1f649ceb0e1115a54965e169532541dc22216a7
dagmarM <Dagmar.Mueller@brockmann-consult.de>
2019-05-31 15:41:26 +0200

    update for use with S2MSI

--- get_bands.py
+++ get_bands.py
@@ -17,11 +17,12 @@ def main(sensor,proc):
         bands_abs = [760, 764, 767, 900, 940]
         #bands_corr = [    412,443,490,510,560,620,665,            754,            779,865                 ] # POLYMER
         #bands_corr = [400,412,443,490,510,560,620,665,674,681,709,754,            779,865,            1020] #C2RCC
-    elif sensor == 'S2':
+    elif sensor == 'S2MSI':
         #TODO add the missing wavelengths
         bands_sat = [443, 490, 560, 665, 705, 740, 783, 842, 865, 945, 1375, 1610, 2190]
         bands_rw  = []
         bands_corr = []
+        bands_chi2 = []
         bands_forwardNN = [443, 490, 560, 665, 705, 740]
         bands_abs = []
     else:
