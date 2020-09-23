c668dbc9ec886d30c0c4739af1cb8644b9c2be64
Soman2 <roman.shevchuk@brockmann-consult.de>
2020-09-23 11:26:13 +0200

    Commented writing dummy file

--- src/main/python/baltic_op.py
+++ src/main/python/baltic_op.py
@@ -22,6 +22,9 @@ class BalticOp:
 
     def __init__(self):
         self.sourceProduct = None
+        self.targetProduct = None
+        self.format = "BEAM-DIMAP"
+        self.outputPath = None
         pass
 
     def initialize(self, operator):
@@ -44,13 +47,21 @@ class BalticOp:
 
         # get  source product:
         sourceProduct = operator.getSourceProduct('sourceProduct')
-        inpath = None
+
         if not sourceProduct:
             raise RuntimeError('No source product specified or product not found - cannot continue.')
         if 'S3A_OL' not in sourceProduct.getName():
             raise RuntimeError('Source product does not seem to be an OLCI L1b product - cannot continue.')
 
-        ######## Copy from breadboard
+        #######
+        f.write("Start initialize: source product is " + sourceProduct.getName() + '\n')
+        print('Start initialize: source product is ' + sourceProduct.getName() + '\n')
+
+
+        width = sourceProduct.getSceneRasterWidth()
+        height = sourceProduct.getSceneRasterHeight()
+        f.write('Source product width, height = ...' + str(width) + ', ' + str(height) + '\n')
+        print('Source product width, height = ...' + str(width) + ', ' + str(height) + '\n')
         #######
         sensor = "OLCI"
         outputSpectral = {'rho_toa': 'rho_toa',
@@ -64,7 +75,8 @@ class BalticOp:
             'log_bpart': 'log_iop[:,3]',
             'log_bwit': 'log_iop[:,4]'
         }
-
+        self.format = operator.getParameter('format')
+        self.outputPath = operator.getParameter('outputPath')
         targetProduct = baltic_AC(sourceProduct=sourceProduct, sensor='OLCI',
                                   addName='_fwNNHL_50x40x40Noise_',
                                   NNversion='TF_n',
@@ -73,23 +85,12 @@ class BalticOp:
                   correction='HYGEOS',
                   add_c2rccIOPs=False)
 
-        #File = jpy.get_type('java.io.File')
-        #GPF.writeProduct(targetProduct, File("C:\dummy.dim"), "BEAM-DIMAP", False, ProgressMonitor.NULL)
+
+        File = jpy.get_type('java.io.File')
+        GPF.writeProduct(targetProduct, File(self.outputPath), self.format, False, ProgressMonitor.NULL)
 
 
-        #######
-        #######
-        f.write("Start initialize: source product is " + sourceProduct.getName() + '\n')
-        print('Start initialize: source product is ' + sourceProduct.getName() + '\n')
 
-
-        width = sourceProduct.getSceneRasterWidth()
-        height = sourceProduct.getSceneRasterHeight()
-        f.write('Source product width, height = ...' + str(width) + ', ' + str(height) + '\n')
-        print('Source product width, height = ...' + str(width) + ', ' + str(height) + '\n')
-
-        #self.baltic_ac_algo = baltic_ac_algorithm.BalticAcAlgorithm()
-        #self.baltic_ac_algo.run(None)
         f.write('end initialize.')
         f.close()
         operator.setTargetProduct(targetProduct)
